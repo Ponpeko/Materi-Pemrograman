eaf1ede407b4c73865daec21a92d8415e401e693
Ponpeko <128123487+Ponpeko@users.noreply.github.com>
2023-09-11 05:34:21 +0700

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,22 +1,36 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Forms;
+using CalculatorMachine;
 
-namespace Pertemuan_1
+namespace CalculatorConsoleApp
 {
-    internal static class Program
+    internal class Program
     {
-        /// <summary>
-        /// The main entry point for the application.
-        /// </summary>
-        [STAThread]
-        static void Main()
+        static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Console.Title = "Aplikasi Calculator Versi Console";
+
+            Console.Write("Nilai A: ");
+            var a = int.Parse(Console.ReadLine());
+
+            Console.Write("Nilai B: ");
+            var b = int.Parse(Console.ReadLine());
+
+            var cal = new Calculator();
+
+            Console.WriteLine();
+            Console.WriteLine("Hasil: ");
+            Console.WriteLine("Penjumlahan: {0} + {1} = {2}", a, b, cal.penjumlahan(a,b));
+            Console.WriteLine("Pengurangan: {0} - {1} = {2}", a, b, cal.pengurangan(a,b));
+            Console.WriteLine("Perkalian  : {0} * {1} = {2}", a, b, cal.perkalian(a,b));
+            Console.WriteLine("Pembagian  : {0} / {1} = {2}", a, b, Calculator.pembagian(a,b));
+            Console.WriteLine("Pangkat    : {0} ^ {1} = {2}", a, b, cal.Pangkat(a,b));
+            Console.WriteLine("Modulo     : {0} % {1} = {2}", a, b, Calculator.Modulo(a,b));
+
+            Console.ReadKey();
         }
     }
 }
